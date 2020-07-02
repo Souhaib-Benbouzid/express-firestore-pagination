4c6c604b5df209faeff9aab650d2683f461b4ae1
Souhaib_benbouzid <54677068+Souhaib-Benbouzid@users.noreply.github.com>
2020-07-02 13:17:00 +0100

    fix image

--- functions/utils/FBAuth.js
+++ functions/utils/FBAuth.js
@@ -30,7 +30,6 @@ exports.FBAuth = (req, res, next) => {
     })
     .then((data) => {
       req.user.handle = data.docs[0].data().handle;
-      req.user.imageUrl = data.docs[0].data().imageUrl;
       return next();
     })
     .catch((err) => {
